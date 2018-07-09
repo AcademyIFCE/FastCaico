0ba7a1a53f0ca009f9d2b06af00118727ba495be
Yuri <yuri@172-16-2-32.0000.ifce.br.iosda.org>
2018-07-09 15:15:18 -0300

    added models protocols

---
+++ FastCaico/Model/Chargeable.swift
@@ -0,0 +1,13 @@
+//
+//  Chargeable.swift
+//  FastCaico
+//
+//  Created by Yuri on 05/07/18.
+//  Copyright © 2018 academy. All rights reserved.
+//
+
+import Foundation
+
+protocol Chargeable {
+    var price: Float { get set }
+}
---
+++ FastCaico/Model/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  FastCaico
+//
+//  Created by Yuri on 05/07/18.
+//  Copyright © 2018 academy. All rights reserved.
+//
+
+import Foundation
+
+protocol Ingredient {
+    var name: String { get set }
+    var image: String { get set }
+}
